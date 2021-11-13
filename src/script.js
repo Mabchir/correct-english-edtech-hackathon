33651e5b188103e02d9f31f8baedd928afc109d7
Mabchir <38863638+Mabchir@users.noreply.github.com>
2021-11-13 08:56:18 -0500

    algorithm

--- src/script.js
+++ src/script.js
@@ -6,4 +6,23 @@ function hidetask() {
   document.getElementById("tasks").style.display = "none";
 }
 
+function handlehide1() {
+  document.getElementById("hide1").style.display = "block";
+}
+function handlehide1right() {
+  document.getElementById("hide1").style.display = "none";
+  document.getElementById("hide2").style.display = "block";
+}
 
+function handlehide1wrong() {
+  document.getElementById("hide1wrong").style.backgroundColor = "red";
+}
+
+function handlehide2no() {
+  document.getElementById("error").style.display = "block";
+}
+
+function handlehide2yes() {
+  document.getElementById("hide2").style.display = "none";
+  document.getElementById("hide3").style.display = "block";
+}
